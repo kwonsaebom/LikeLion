9396c1b04f6d6b32b3d9a7a866426bc6db9703f8
Saebom <spring180@naver.com>
2024-09-04 13:29:27 +0900

    Test: Code Convention

---
+++ JavaScript/Day2/test1/main.js
@@ -0,0 +1,27 @@
+//다음의 함수는 1부터 10까지 숫자를 더하는 함수입니다.
+const sum = () => {
+  let num = 0;
+
+  for (i = 1; i <= 10; i++) {
+    num += i;
+    console.log("sum", num);
+  }
+};
+sum();
+
+//다음의 코드는 클래스를 생성하고 클래스의 함수를 호출하는 코드입니다.
+class USER {
+  NAME = "홍길동";
+  sayHello = () => {
+    console.log(`hello, ${this.NAME}`);
+  };
+}
+new USER().sayHello();
+
+//다음의 코드는 함수를 호출하여 상품을 추가하는 코드입니다.
+let productNumber = 0;
+
+const addProduct = () => {
+  console.log(`${productNumber} 상품을 추가했습니다.`);
+};
+addProduct();
