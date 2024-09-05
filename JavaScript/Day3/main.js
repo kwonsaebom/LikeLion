7b84ce265a411513469f0a12cadf42413c4bdcad
Saebom <spring180@naver.com>
2024-09-05 10:49:57 +0900

    Introduce Myself

---
+++ JavaScript/Day3/main.js
@@ -0,0 +1,11 @@
+const NAME = "권새봄";
+let introduce = "안녕하세요 프론트엔드 개발자를 꿈으로 하는 권새봄입니다!";
+let wish =
+  "기본기를 탄탄하게 다지고 그것을 적용할 수 있는 개발자가 되고 싶습니다";
+let message =
+  "열정 많은 사람들과 함께 같은 목표를 가지고 열심히 임해보고 싶습니다 화이팅!!";
+
+document.getElementById("name").textContent = NAME;
+document.getElementById("introduce").textContent = introduce;
+document.getElementById("wish").textContent = wish;
+document.getElementById("message").textContent = message;
