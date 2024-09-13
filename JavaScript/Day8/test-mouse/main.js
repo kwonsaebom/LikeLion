3ee8f6f82971a1185e62be20f69a50d7c47a2c67
Saebom <spring180@naver.com>
2024-09-13 11:14:16 +0900

    test - answer

--- JavaScript/Day8/test-mouse/main.js
+++ JavaScript/Day8/test-mouse/main.js
@@ -1,39 +1,47 @@
-const inputId = document.querySelector("#ID");
-const inputPw = document.querySelector("#PW");
 const idMsg = document.querySelector(".id-msg");
 const pwMsg = document.querySelector(".pw-msg");
-const resultMsg = document.querySelector(".result-msg");
 
-const loginButton = document.querySelector(".login");
-const form = document.querySelector(".form");
+let isIdValid = false;
+let isPwValid = false;
 
-function inputText(node) {
-  if (node === idMsg) {
-    node.textContent = "아이디는 필수 입력입니다";
-  } else if (node === pwMsg) {
-    node.textContent = `비밀번호는 6~16자리, 영문 및 숫자를 포함해야 합니다`;
-  } else if (node === resultMsg) {
-    node.textContent = `${inputId.value}님이 로그인을 시도합니다.`;
+let id = "";
+let pw = "";
+
+function idValidCheck() {
+  const idNode = document.querySelector("#ID");
+  id = idNode.value;
+
+  if (id === null || id.trim().length == 0) {
+    isIdValid = false;
+    idMsg.innerHTML = "아이디를 입력하세요.";
+  } else {
+    isIdValid = true;
+    idMsg.innerHTML = "";
   }
 }
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
 
-  let regExgPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,16}$/;
+function pwValidCheck() {
+  const regExgPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{6,16}$/;
 
-  idMsg.textContent = "";
-  pwMsg.textContent = "";
-
-  if (inputId.value === "" && !regExgPassword.test(inputPw.value)) {
-    inputText(idMsg);
-    inputText(pwMsg);
-  } else if (inputId.value === "") {
-    regExgPassword.test(inputPw.value);
-    inputText(idMsg);
-  } else if (!regExgPassword.test(inputPw.value)) {
-    inputText(pwMsg);
+  const pwNode = document.querySelector("#PW");
+  pw = pwNode.value;
+  if (pw === null || pw.trim().length === 0) {
+    isPwValid = false;
+    pwMsg.innerHTML = "패스워드를 입력하세요";
+  } else if (!regExgPassword.test(pw)) {
+    isPwValid = false;
+    pwMsg.innerHTML = "패스워드는 영문자, 숫자 조합에 6자 이상입니다.";
   } else {
-    inputText(resultMsg);
-    document.body.appendChild(resultMsg);
+    isPwValid = true;
+    pwMsg.innerHTML = "";
   }
-});
+}
+
+function login() {
+  idValidCheck();
+  pwValidCheck();
+  if (isIdValid && isPwValid) {
+    let resultMsg = document.querySelector(".result-msg");
+    resultMsg.innerHTML = `${id}님이 로그인을 시도합니다`;
+  }
+}
