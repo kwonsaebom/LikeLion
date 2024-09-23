fa2c08cd12b839c77c6e1ea56544c9b9e9e32410
Saebom <spring180@naver.com>
2024-09-23 11:15:55 +0900

    Day11

---
+++ JavaScript/Day11/style/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let area1 = document.getElementById("area1");
+let area2 = document.getElementById("area2");
+let area3 = document.getElementById("area3");
+console.log(area1.style.width);
+console.log(area2.style.width);
+console.log(area3.style.width);
