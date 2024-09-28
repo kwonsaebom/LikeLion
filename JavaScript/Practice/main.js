0a235c3f00cd2e90438dcde76c927e6ee492256a
Saebom <spring180@naver.com>
2024-09-28 20:00:21 +0900

    총액 계산x

--- JavaScript/Practice/main.js
+++ JavaScript/Practice/main.js
@@ -1,4 +1,6 @@
 const selectNode = document.getElementById("dropdown");
+const container = document.querySelector(".container");
+const buyButton = document.getElementById("buy");
 
 function Product(name, price) {
   this.name = name;
@@ -15,11 +17,40 @@ let products = [
 console.log(selectNode);
 
 products.forEach((product) => {
-  optionNode = document.createElement("option");
+  let optionNode = document.createElement("option");
   optionNode.textContent = `${product.name} - ${product.price}`;
+  optionNode.value = product.name;
   selectNode.appendChild(optionNode);
 });
 
-console.log(products);
-console.log(products);
-console.log(products);
+// 구매 버튼 클릭 시 선택된 상품을 출력
+buyButton.addEventListener("click", () => {
+  const selectedOption = selectNode.options[selectNode.selectedIndex];
+  const selectedIndex = selectNode.selectedIndex;
+
+  if (selectedIndex === -1) {
+    alert("결제할 상품을 선택해야 합니다.");
+    return;
+  }
+
+  const selectedProduct = products.find(
+    (product) => product.name === selectedOption.value
+  );
+
+  if (selectedProduct) {
+    const chooseNode = document.createElement("h1");
+    chooseNode.textContent = "선택한 상품";
+    container.appendChild(chooseNode);
+
+    const listNode = document.createElement("ul");
+    listNode.innerHTML = `
+    <li>${selectedProduct.name} - ${selectedProduct.price}</li>`;
+    container.appendChild(listNode);
+
+    const priceNode = document.createElement("h1");
+    priceNode.textContent = "총액: ";
+    document.body.appendChild(priceNode);
+  } else {
+    console.log("No product selected");
+  }
+});
