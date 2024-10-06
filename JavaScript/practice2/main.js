cc6582560e15c2a10d3cf943f239d7146e069b47
Saebom <spring180@naver.com>
2024-10-06 15:48:29 +0900

    과제2

--- JavaScript/practice2/main.js
+++ JavaScript/practice2/main.js
@@ -1,16 +1,58 @@
 const searchButton = document.querySelector("#search");
+const inputNode = document.querySelector("#input-box");
 const resultBox = document.querySelector(".result-container");
 const API_KEY = "98e79f5f8ca244ce994fa51568399af3";
-var url =
-  "https://newsapi.org/v2/everything?" +
-  "q=Apple&" +
-  "from=2024-10-04&" +
-  "sortBy=popularity&" +
-  "apiKey=${API_KEY}";
 
-var req = new Request(url);
+searchButton.addEventListener("click", () => {
+  const search = inputNode.value;
 
-fetch(req).then(function (response) {
-  console.log(response.json());
-  console.log(response.json());
+  if (search.trim().length === 0) {
+    alert("검색어를 입력하세요!");
+    return;
+  }
+
+  resultBox.innerHTML = "";
+
+  let url =
+    "https://newsapi.org/v2/everything?" +
+    `q=${search}&` +
+    "sortBy=popularity&" +
+    `apiKey=${API_KEY}`;
+
+  searchWord();
+
+  async function searchWord() {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+
+    if (data.totalResults === 0) {
+      alert("검색 결과가 없습니다!");
+      return;
+    }
+
+    data.articles.forEach((article) => {
+      if (
+        article.title.trim() !== "[Removed]" &&
+        article.author &&
+        article.urlToImage
+      ) {
+        console.log(article);
+
+        const newNode = document.createElement("div");
+        newNode.setAttribute("class", "article-container");
+        newNode.innerHTML = `
+        <div class="container-left">
+          <h3 class="article-title">${article.title}</h3>
+          <i class="article-info">${article.author} - ${article.publishedAt} - <a href="${article.url}">more</a></i>
+          <p class="article-content">${article.content}</p>
+        </div>
+        <div class="container-right">
+          <img class="article-image" src="${article.urlToImage}"/>
+        </div>`;
+
+        resultBox.appendChild(newNode);
+      }
+    });
+  }
 });
